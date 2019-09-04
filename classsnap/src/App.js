256ff39298d74fdd8083a573916190e0f1e3f9ef
karenjli <karenjy.li@gmail.com>
2019-09-03 22:28:00 -0500

    built signin form with formik

--- classsnap/src/App.js
+++ classsnap/src/App.js
@@ -2,17 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-
 //components
 import Login from "./components/SignUp_Pages/User-signin";
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
         <Login />
+        <h1>Test</h1>
       </header>
     </div>
   );
