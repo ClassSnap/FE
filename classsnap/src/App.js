b493f4cc1ce516bb2d5c6d065a11c26b7772fa55
karenjli <karenjy.li@gmail.com>
2019-09-06 21:53:41 -0500

    question form built

--- classsnap/src/App.js
+++ classsnap/src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 
 //components
 import Login from "./components/SignUp_Pages/User-signin";
+import AddQuestionForm from "./components/Teacher-Dashboard/AddQuestionForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Login />
+        <AddQuestionForm />
         <h1>Test</h1>
       </header>
     </div>
