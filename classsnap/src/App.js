40db12bde7a9515f6b23e49c9e5d199ea7ac93d3
karenjli <karenjy.li@gmail.com>
2019-09-06 23:07:01 -0500

    parent response form built

--- classsnap/src/App.js
+++ classsnap/src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 //components
 import Login from "./components/SignUp_Pages/User-signin";
 import AddQuestionForm from "./components/Teacher-Dashboard/AddQuestionForm";
+import ParentResponseForm from "./components//Parent-Dashboard/Parent-ResponseForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Login />
-        <AddQuestionForm />
-        <h1>Test</h1>
-      </header>
+      <Login />
+      <AddQuestionForm />
+      <ParentResponseForm />
     </div>
   );
 }
