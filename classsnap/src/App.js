e7189d88f73dadadcc9a838e4853246673539bf4
karenjli <karenjy.li@gmail.com>
2019-09-18 23:00:59 -0500

    styling for navbar and add question form completed

--- classsnap/src/App.js
+++ classsnap/src/App.js
@@ -11,39 +11,46 @@ import ParentNewQuestionList from "./components/Parent-Dashboard/Parent-NewQuest
 import ParentAnsweredQuestionList from "./components/Parent-Dashboard/Parent-AnsweredQuestionList";
 import QuestionResult from "./components/Teacher-Single-Class-Dashboard/QuestionResult";
 import QuestionResultList from "./components/Teacher-Single-Class-Dashboard/QuestionResultList";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>ClassSnap in Progress</h1>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/teacher/add-question" component={AddQuestionForm} />
-        <Route
-          exact
-          path="/teacher/question-result-list"
-          component={QuestionResultList}
-        />
-        <Route
-          exact
-          path="/teacher/question-result"
-          component={QuestionResult}
-        />
-        <Route
-          exact
-          path="/parent/response-form"
-          component={ParentResponseForm}
-        />
-        <Route
-          exact
-          path="/parent/new-question-list"
-          component={ParentNewQuestionList}
-        />
-        <Route
-          exact
-          path="/parent/answered-question-list"
-          component={ParentAnsweredQuestionList}
-        />
+        <NavBar />
+        <div className="container">
+          <Route exact path="/login" component={Login} />
+          <Route
+            exact
+            path="/teacher/add-question"
+            component={AddQuestionForm}
+          />
+          <Route
+            exact
+            path="/teacher/question-result-list"
+            component={QuestionResultList}
+          />
+          <Route
+            exact
+            path="/teacher/question-result"
+            component={QuestionResult}
+          />
+          <Route
+            exact
+            path="/parent/response-form"
+            component={ParentResponseForm}
+          />
+          <Route
+            exact
+            path="/parent/new-question-list"
+            component={ParentNewQuestionList}
+          />
+          <Route
+            exact
+            path="/parent/answered-question-list"
+            component={ParentAnsweredQuestionList}
+          />
+        </div>
       </div>
     </Router>
   );
--- classsnap/src/components/Teacher-Dashboard/AddQuestionForm.js
+++ classsnap/src/components/Teacher-Dashboard/AddQuestionForm.js
@@ -1,49 +1,55 @@
 import React from "react";
-import { Form, Field, withFormik, yupToFormErrors } from "formik";
+import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
 const AddQuestionForm = () => {
   return (
     <div className="question-form">
-      <Form>
-        <h2>Send your Question Here!</h2>
-        <label>
-          Class
-          <Field component="select" name="session">
-            <option value="classOne">Class 1</option>
-            <option value="classTwo">Class 2</option>
-            <option value="classThree">Class 3</option>
-            <option value="classFour">Class 4</option>
-            <option value="classFive">Class 5</option>
-            <option value="classSix">Class 6</option>
-            <option value="classSeven">Class 7</option>
-            <option value="classEight">Class 8</option>
-          </Field>
-        </label>
-        <label>
-          Subject
-          <Field component="select" name="subject">
-            <option value="elar">ELAR</option>
-            <option value="math">Math</option>
-            <option value="sci">Science</option>
-            <option value="socio">Social Studies</option>
-            <option value="lang">Foreign Languages</option>
-          </Field>
-        </label>
-        <label>
-          Due Date
-          <Field type="date" name="duedate" />
-        </label>
-        <label>
-          Question
-          <Field
-            type="text"
-            name="question"
-            placeholder="Enlightening question starts here..."
-          />
-        </label>
-        <button type="submit">Submit</button>
+      <Form className="form">
+        <h2>Add Question</h2>
+        <div className="basic-info">
+          <div className="class">
+            <label>Class</label>
+            <Field component="select" name="session" class="option">
+              <option value="default">Select</option>
+              <option value="classOne">Class 1</option>
+              <option value="classTwo">Class 2</option>
+              <option value="classThree">Class 3</option>
+              <option value="classFour">Class 4</option>
+              <option value="classFive">Class 5</option>
+              <option value="classSix">Class 6</option>
+              <option value="classSeven">Class 7</option>
+              <option value="classEight">Class 8</option>
+            </Field>
+          </div>
+          <div className="subject">
+            <label>Subject </label>
+            <Field component="select" name="subject" class="option">
+              <option value="select">Select</option>
+              <option value="elar">ELAR</option>
+              <option value="math">Math</option>
+              <option value="sci">Science</option>
+              <option value="socio">Social Studies</option>
+              <option value="lang">Foreign Languages</option>
+            </Field>
+          </div>
+          <div className="date">
+            <label>Due Date</label>
+            <Field type="date" name="duedate" />
+          </div>
+        </div>
+
+        <Field
+          type="text"
+          name="question"
+          class="inputQ"
+          placeholder="Enlightening question starts here..."
+        />
+
+        <button className="submit" type="submit">
+          Submit
+        </button>
       </Form>
     </div>
   );
